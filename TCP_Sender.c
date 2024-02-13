0c3b52b437863e137983143b0a2f7493b177de34
IdoMangadi <idomangadi@gmail.com>
2024-02-13 22:09:52 +0200

    sender adjusment

--- TCP_Sender.c
+++ TCP_Sender.c
@@ -35,36 +35,52 @@ char *util_generate_random_data(unsigned int size){
 
 
 int main(){
-    int sock = -1;
-    struct  sockaddr_in server;
-    memset(&server, 0, sizeof(server));
+    unsigned int size = 3 * 1024 * 1024;
+    char *random_data = util_generate_random_data(size);
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if ( sock == -1){
-        perror("Error occured whlie creating socket");
-        return 1;
+    if (random_data != NULL){
+
+
+        int sock = -1;
+        struct  sockaddr_in server;
+        memset(&server, 0, sizeof(server));
+
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if ( sock == -1){
+            perror("Error occured whlie creating socket");
+            return 1;
+        }
+
+        if (inet_pton(AF_INET, "127.0.0.1", &server.sin_addr) <= 0){
+            perror("Error - invalid address / addres not supported");
+            return 1;
+        }
+
+        server.sin_family = AF_INET;
+        server.sin_port = htons(SERVER_PORT);
+
+        if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0){
+            perror("Connection failed");
+            return 1;
+        }
+
+        int user_choice = 1;
+        while(user_choice){
+            // Sending the file:
+            if (send(sock, random_data, size, 0) < 0){
+                perror("Send failed");
+                return 1;
+            }
+            // Asking the user about sending again:
+            do{
+                printf("Do you want to send the file again?\nYes -> Enter 1\nNo -> Enter 0\nYour answer: ");
+            } while(scanf(" %d", &user_choice) != 1);
+        }
+
+        close(sock);
+        free(random_data);
+
     }
-
-    if (inet_pton(AF_INET, "127.0.0.1", &server.sin_addr) <= 0){
-        perror("Error - invalid address / addres not supported");
-        return 1;
-    }
-
-    server.sin_family = AF_INET;
-    server.sin_port = htons(SERVER_PORT);
-
-    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0){
-        perror("Connection failed");
-        return 1;
-    }
-
-    char *massage = "Hello";
-    if (send(sock, massage, strlen(massage)+1, 0) < 0){
-        perror("Send failed");
-        return 1;
-    }
-
-    close(sock);
 
     return 0;
 }
